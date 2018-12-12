c5550c7e2c4298525becbcc10496454ce0e16e7c
Ashhad khan <khanrider44@gmail.com>
2018-12-12 07:46:45 +0100

    as

--- scrape.php
+++ scrape.php
@@ -5,35 +5,28 @@ require_once('simplehtmldom_1_5/simple_html_dom.php');
 function findAndCompare(){
 	//storing user inputs into variables
 	//$uri = urlencode('http://...')
-	$context = stream_context_create(array(
+	/*$context = stream_context_create(array(
     'http' => array(
         'header' => array('User-Agent: Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201'),
     ),
-	));
-	
-/*$curl = curl_init();
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
-curl_setopt($curl, CURLOPT_HEADER, false);
-curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($curl, CURLOPT_URL, "https://whatismyip.com");
-curl_setopt($curl, CURLOPT_REFERER, "https://whatismyip.com");
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201');
-$str = curl_exec($curl);
-curl_close($curl);*/
-	
+	));*/
+
+	$opts = array('http'=>array('header' => "User-Agent:Mozilla/5.0 (Windows NT 6.2) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.75 Safari/537.1\r\n"));
+	$context = stream_context_create($opts);
+
 	
 	
 	$url1 = $_POST['website_url1'];
 	$url2 = $_POST['website_url2'];
 	if(!empty($url1)&&!empty($url2)){
 		
-		$html1 = file_get_html($url1, false, $context);
-		$html2 = file_get_html($url2, false, $context);
+		$html1 = str_get_html(file_get_contents($url1, false, $context));
+		$html2 = str_get_html(file_get_contents($url2, false, $context));
 		
 		if(!empty($html1&&!empty($html2))){
 			$url1_links = array();
 			$url2_links = array();
+			if(is_object($html1)){
 				foreach($html1->find('a[href^="http://www.tuttosport.com"]') as $a)
 				{
 					if($a->href)
@@ -41,6 +34,8 @@ curl_close($curl);*/
 						$url1_links[]=$a->href;
 					}
 				}	
+			}
+			if(is_object($html2)){
 				foreach($html2->find('a[href^="http://www.gazzetta.it"]') as $a)
 				{
 					if($a->href) 
@@ -48,7 +43,8 @@ curl_close($curl);*/
 						$url2_links[]=$a->href;
 					}
 				}	
-			
+			}
+	
 			
 			$output = fopen('php://output', 'w');
 			foreach($url1_links as $val1)
